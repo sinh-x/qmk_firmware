bf624489775e4935d20fec97a5e6a106c585eadf
Sinh Nguyen <sinh@sinhn.com>
2024-05-30 22:20:58 +0700

    Fixed typos

--- keyboards/sofle/keymaps/sinh-x/keymap.c
+++ keyboards/sofle/keymaps/sinh-x/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                 KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   KC_GRV,    KC_PLUS,    KC_7,    KC_8,    KC_9,    KC_ASTR,            KC_LBRC,  KC_DLR, KC_LPRN, KC_RPRN,  KC_AMPR,  KC_F12,
-  _______, KKC_MINS,  KC_4, KC_5,  KC_6, KC_SLSH,                       KC_RBRC,  KC_AT, KC_COMM, KC_DOT, KC_PIPE, _______,
+  _______, KC_MINS,  KC_4, KC_5,  KC_6, KC_SLSH,                       KC_RBRC,  KC_AT, KC_COMM, KC_DOT, KC_PIPE, _______,
   _______,  KC_EQL, KC_1, KC_2, KC_3, KC_0, _______,           _______, KC_CIRC,  KC_HASH, KC_LT, KC_GT, KC_BSLS, _______,
                        _______, _______, _______, _______, TL_LOWR,       TL_UPPR, _______, _______, _______, _______
 ),
