f14de814b134288963230a592c6cc6d88f606210
Sinh Nguyen <sinh@sinhn.com>
2025-06-05 12:48:29 +0700

    Update sofle keymap

--- keyboards/sofle/keymaps/sinh-x/keymap.c
+++ keyboards/sofle/keymaps/sinh-x/keymap.c
@@ -58,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //        ├────────┼───┼───────────┼───────────┼───────────┼────────────┼───────┐   ┌───────┼─────────────┼───────────┼───────────┼───────────┼───┼──────┤
 //        │ SH_MON │ x │     c     │     d     │     v     │     z      │ mute  │   │  no   │      /      │     k     │     h     │     ,     │ . │ del  │
 //        └────────┴───┼───────────┼───────────┼───────────┼────────────┼───────┤   ├───────┼─────────────┼───────────┼───────────┼───────────┼───┴──────┘
-//                     │  TL_UPPR  │    no     │   TT(5)   │ LT(4, spc) │ MO(1) │   │ MO(2) │ LSFT_T(ent) │   TT(4)   │  SH_TOGG  │  TL_LOWR  │
+//                     │  TL_UPPR  │   TT(5)   │   MO(2)   │ LT(4, spc) │ MO(1) │   │ MO(2) │ LSFT_T(ent) │   TT(4)   │  SH_TOGG  │  TL_LOWR  │
 //                     └───────────┴───────────┴───────────┴────────────┴───────┘   └───────┴─────────────┴───────────┴───────────┴───────────┘
 [_COLEMAK] = LAYOUT_sinh_x_58(
       KC_GRV , KC_1 , KC_2         , KC_3         , KC_4         , KC_5          ,                         KC_6           , KC_7         , KC_8         , KC_9         , KC_0    , KC_EQL ,
       KC_TAB , KC_Q , KC_W         , KC_F         , KC_P         , KC_B          ,                         KC_J           , KC_L         , KC_U         , KC_Y         , KC_SCLN , KC_QUOT,
       KC_ESC , KC_A , LCTL_T(KC_R) , LALT_T(KC_S) , LSFT_T(KC_T) , LGUI_T(KC_G)  ,                         RGUI_T(KC_M)   , RSFT_T(KC_N) , RALT_T(KC_E) , RCTL_T(KC_I) , KC_O    , KC_BSPC,
       SH_MON , KC_X , KC_C         , KC_D         , KC_V         , KC_Z          , KC_MUTE ,     XXXXXXX , KC_SLSH        , KC_K         , KC_H         , KC_COMM      , KC_DOT  , KC_DEL ,
-                      TL_UPPR      , XXXXXXX      , TT(5)        , LT(4, KC_SPC) , MO(1)   ,     MO(2)   , LSFT_T(KC_ENT) , TT(4)        , SH_TOGG      , TL_LOWR
+                      TL_UPPR      , TT(5)        , MO(2)        , LT(4, KC_SPC) , MO(1)   ,     MO(2)   , LSFT_T(KC_ENT) , TT(4)        , SH_TOGG      , TL_LOWR
 ),
 
 //        ┌──────┬────┬───────────┬───────────┬───────────┬───────────┐                       ┌───────────┬───────────┬───────────┬───────────┬─────┬─────┐
